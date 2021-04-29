7deab94b95e75540d004971dc37931b07680f613
Azamat Bayev <azamat.bayev1@gmail.com>
2021-04-29 11:18:49 +0200

    Initial commit

---
+++ InsertDataWeatherApp/Migrations/20210127095413_20210127.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InsertDataWeatherApp.Migrations
+{
+    public partial class _20210127 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WeatherDataInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateAndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Temp = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Humidity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherDataInfo", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeatherDataInfo");
+        }
+    }
+}
---
+++ InsertDataWeatherApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using InsertDataWeatherApp.Entities;
+using InsertDataWeatherApp.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace InsertDataWeatherApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainMenuMethods.Run();
+        }
+    }
+        
+}
